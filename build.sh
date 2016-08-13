52c7d16ed477a35c6a58b57e7e50b7aba5fabc39
Alex Bello <soue7@hotmail.com>
2016-08-13 02:50:30 -0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#exit if any command fails
+set -e
+
+artifactsFolder="./artifacts"
+
+if [ -d $artifactsFolder ]; then
+  rm -R $artifactsFolder
+fi
+
+dotnet restore
+
+dotnet test
